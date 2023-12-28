98caf70763b70e572861afb104119badb0d950fc
Gwen <tallrainfall@protonmail.com>
2023-12-28 11:02:16 -0500

    update a bunch of stuff

--- .config/swaylock/lock.sh
+++ .config/swaylock/lock.sh
@@ -17,46 +17,46 @@
 
 original_dir="$(pwd)"
 
-cd "$(dirname "$0")" || exit
+#cd "$(dirname "$0")" || exit
 
-grim -t jpeg screen.jpg
+#grim -t jpeg screen.jpg
 
 #get logo path, if none random
-if [[ "$2" != "" ]]; then
-  cd "$original_dir" || exit
-  image=$(realpath "$2")
-  cd "$(dirname "$0")" || exit
-else
-  image="icons/$(shuf -i0-2 -n1).png"
-fi
+#if [[ "$2" != "" ]]; then
+#  cd "$original_dir" || exit
+#  image=$(realpath "$2")
+#  cd "$(dirname "$0")" || exit
+#else
+#  image="icons/$(shuf -i0-2 -n1).png"
+#fi
 
-image="icons/3.png"
+#image="icons/3.png"
 
 #make background image
-rm logo-ed_screen.png
+#rm logo-ed_screen.png
 
 # this command is crazy fast, but it gaussian blurs and overlays a logo 
 # with a speed that is around 3-4 times faster than the time it takes for
 # imagemagick to simply composite that logo (no blurring)
 # (test is not done in a controlled environment, but you get the idea)
 # I FUCKING LOVE FFMPEG!!
-ffmpeg -i "$HOME/Backgrounds/ink.jpeg" -vf \
-  "[in] gblur=sigma=$1 [out]" \
-  logo-ed_screen.png
+#ffmpeg -i "$HOME/Backgrounds/ink.jpeg" -vf \
+#  "[in] gblur=sigma=$1 [out]" \
+#  logo-ed_screen.png
 
 #import pywal colors
 # shellcheck source=/home/lfron/.cache/wal/colors.sh
 source "$HOME/.cache/wal/colors.sh"
 
 swaylock \
-  --image "$HOME/.config/swaylock/logo-ed_screen.png" \
+  --image "$wallpaper" \
   --daemonize \
   --indicator-radius 160 \
   --indicator-thickness 20 \
-  --inside-color 00000000 \
-  --inside-clear-color 00000000 \
-  --inside-ver-color 00000000 \
-  --inside-wrong-color 00000000 \
+#  --inside-color 00000000 \
+#  --inside-clear-color 00000000 \
+#  --inside-ver-color 00000000 \
+#  --inside-wrong-color 00000000 \
   --key-hl-color "$color1" \
   --bs-hl-color "$color2" \
   --ring-color "$background" \
@@ -64,13 +64,13 @@ swaylock \
   --ring-wrong-color "$color5" \
   --ring-ver-color "$color3" \
   --line-uses-ring \
-  --line-color 00000000 \
-  --font 'NotoSans Nerd Font Mono:style=Thin,Regular 40' \
-  --text-color 00000000 \
+#  --line-color 00000000 \
+#  --font 'Noto Sans Regular 12' \
+#  --text-color 00000000 \
   --text-clear-color 00000000 \ #"$color2" \
   --text-wrong-color 00000000 \ #"$color5" \
   --text-ver-color 00000000 \ #"$color4" \
   --separator-color 00000000 \
   --grace 10 \
   --grace-no-mouse \
-  --ignore-empty-password \
+  --ignore-empty-password
