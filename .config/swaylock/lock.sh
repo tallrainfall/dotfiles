92d6b8def95d311dcc4fa7a6f2d46f142b87a2ce
Gwen <tallrainfall@protonmail.com>
2024-02-04 12:08:41 -0500

    stuff

--- .config/swaylock/lock.sh
+++ .config/swaylock/lock.sh
@@ -13,57 +13,17 @@
 #   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 # 
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
-
-
-original_dir="$(pwd)"
-
-#cd "$(dirname "$0")" || exit
-
-#grim -t jpeg screen.jpg
-
-#get logo path, if none random
-#if [[ "$2" != "" ]]; then
-#  cd "$original_dir" || exit
-#  image=$(realpath "$2")
-#  cd "$(dirname "$0")" || exit
-#else
-#  image="icons/$(shuf -i0-2 -n1).png"
-#fi
-
-#image="icons/3.png"
-
-#make background image
-#rm logo-ed_screen.png
-
-# this command is crazy fast, but it gaussian blurs and overlays a logo 
-# with a speed that is around 3-4 times faster than the time it takes for
-# imagemagick to simply composite that logo (no blurring)
-# (test is not done in a controlled environment, but you get the idea)
-# I FUCKING LOVE FFMPEG!!
-#ffmpeg -i "$HOME/Backgrounds/ink.jpeg" -vf \
-#  "[in] gblur=sigma=$1 [out]" \
-#  logo-ed_screen.png
-
-#import pywal colors
-# shellcheck source=/home/lfron/.cache/wal/colors.sh
 source "$HOME/.cache/wal/colors.sh"
 
 swaylock \
   --image "$wallpaper" \
   --daemonize \
-  --indicator-radius 160 \
-  --indicator-thickness 20 \
   --key-hl-color "$color1" \
   --bs-hl-color "$color2" \
   --ring-color "$background" \
   --ring-clear-color "$color2" \
   --ring-wrong-color "$color5" \
   --ring-ver-color "$color3" \
-  --line-uses-ring \
-  --text-clear-color 00000000 \
-  --text-wrong-color 00000000 \
-  --text-ver-color 00000000 \
-  --separator-color 00000000 \
   --grace 10 \
   --grace-no-mouse \
   --ignore-empty-password
--- .zprofile
+++ .zprofile
@@ -11,13 +11,16 @@ export NUGET_PACKAGES="$XDG_CACHE_HOME"/NuGetPackages
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR"/gcr/ssh
 export WINEPREFIX="$XDG_DATA_HOME"/wine
 export QT_QPA_PLATFORM="wayland;xcb"
-export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_QPA_PLATFORMTHEME=qt6ct
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export CLUTTER_BACKEND=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_DBUS_REMOTE=1
 export DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.i686.json:/usr/share/vulkan/icd.d/radeon_icd.x86_64.json
 export VDPAU_DRIVER=radeonsi
-#if [ -z "${WAYLAND_DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-#  exec sway
-#fi
+if [ -z "${WAYLAND_DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+  exec sway
+fi
